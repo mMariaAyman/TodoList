36a508e03740694288565eae800fe97b3c794a30
Maria <mera.ayman11@gmail.com>
2024-07-08 05:44:45 +0300

    TodoListlogin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
+
 }
